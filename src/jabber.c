676805b60813885d1add56977e0d88d686453b0e
Richard Holme <richard.holme@arris.com>
2016-10-07 12:46:27 +0100

    Added Copyright notices for bug fixed code

--- src/jabber.c
+++ src/jabber.c
@@ -3,6 +3,7 @@
 ** This code is free software; you can redistribute it and/or
 ** modify it under the terms of GNU Lesser General Public License.
 */
+/* Copyright 2015-2016 ARRIS Enterprises, LLC. */
 
 #include "common.h"
 #include "iksemel.h"
